5d47a642baf8d79f758c21a87e5aaf846a37ace2
Vicki Chen <vickichen@Vickis-Air-2.lan>
2024-05-22 18:19:42 -0400

    add basic search bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
 // import geocodeAddress from './geocode'; // Assuming you have defined this function
-import searchPOI from './searchPOI'; // Import the searchPOI function from the appropriate module
+// import searchPOI from './searchPOI'; // Import the searchPOI function from the appropriate module
 import './App.css';
+import { Img } from 'react-image';
+
 // const { REACT_APP_MAPBOX_API_KEY } = process.env;
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import geocodeAddress from './geocode';
 
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_KEY
@@ -11,6 +17,8 @@ const App = ({ jsonDataArray }) => {
   const mapContainerRef = useRef(null);
   const mapRef = useRef(null);
   const [pins, setPins] = useState([]);
+  const [value, setValue] = React.useState('');
+
 
   useEffect(() => {
     const initializeMap = async () => {
@@ -28,6 +36,34 @@ const App = ({ jsonDataArray }) => {
       });
 
       mapRef.current = map;
+
+      // Add search bar (Mapbox Geocoder)
+     const geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl,
+      marker: false, // Do not add a marker automatically
+    });
+
+    // Add geocoder to map
+    map.addControl(geocoder);
+
+    geocoder.on('result', (event) => {
+      const { result } = event;
+      const [longitude, latitude] = result.center;
+
+      const marker = new mapboxgl.Marker({"color":"red"})
+        .setLngLat([longitude, latitude])
+        .addTo(map);
+
+        marker.getElement().addEventListener('mouseenter', () => {
+          
+        });
+
+        marker.getElement().addEventListener('mouseleave', () => {
+          marker.remove()
+        });
+      });
+
 
       // Fetch coordinates for each item and set pins
       const fetchCoordinatesAndSetPins = async () => {
@@ -64,24 +100,33 @@ const App = ({ jsonDataArray }) => {
       // console.log(item.restaurant,coordinates)
       coordinates = {longitude:item.longitude , latitude:item.latitude};
 
-    } else if (item.restaurant !== " NA" && item.address === " NA") {
-      // If address is not available but restaurant name is provided,
-      // perform a point-of-interest (POI) search
-      // console.log("POI" ,item.restaurant)
-      coordinates = await searchPOI(item.restaurant);
-    }
+    } 
+    // else if (item.restaurant !== " NA" && item.address === " NA") {
+    //   // If address is not available but restaurant name is provided,
+    //   // perform a point-of-interest (POI) search
+    //   // console.log("POI" ,item.restaurant)
+    //   coordinates = await searchPOI(item.restaurant);
+    // }
 
     return coordinates;
   };
 
   useEffect(() => {
+    const MyLoader = () => <div>Loading...</div>;
+    const MyErrorComponent = () => <div>Error loading image.</div>;
+
     if (pins.length > 0 && mapRef.current) {
       pins.forEach((item) => {
         const popupContent = `
           <div class="flex-container">
-            <img src="${item.videoMeta.coverUrl}" alt="Cover" class="popup-image" style="max-width: 300px;"/>
+          <Img
+                src="${item.videoMeta.coverUrl}"
+                loader=${<MyLoader />}
+                unloader=${<MyErrorComponent />}
+                alt="Cover" class="popup-image" style="max-width: 300px;"
+            />
             <video class="popup-video" style="display:none;max-width: 300px;" controls>
-              <source src="${item.videoMeta.downloadAddr}" type="video/mp4">
+              <source preload="none" src="${item.videoMeta.downloadAddr}" type="video/mp4">
             </video>
             <div>
               <p><strong>Search Query:</strong> ${item.searchQuery}</p>
@@ -106,6 +151,7 @@ const App = ({ jsonDataArray }) => {
           const popupEl = document.querySelector('.mapboxgl-popup-content');
           const image = popupEl.querySelector('.popup-image');
           const video = popupEl.querySelector('.popup-video');
+          video.load()
 
           image.addEventListener('mouseenter', () => {
             if (video.paused) {
@@ -130,7 +176,9 @@ const App = ({ jsonDataArray }) => {
   }, [pins]);
 
 
-  return <div ref={mapContainerRef} className="map-container" />;
+  return <div>
+      <div ref={mapContainerRef} className="map-container" value={value}/>
+    </div>;
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <App jsonDataArray={jsonDataArray} />, document.getElementById('root'));
+    <App jsonDataArray={jsonDataArray} />
 
   </React.StrictMode>
 );
