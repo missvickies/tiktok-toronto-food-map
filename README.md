70613e5a2e2b31501c268d10e02a9bbcd4a0d74d
Vicki Chen <vickichen@Vickis-MacBook-Air-2.local>
2024-07-05 19:29:45 -0400

    ui redesign

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ this project displays crawled TikTok's using Apify. It maps out several tiktok f
 
 <img width="1234" alt="IMG_6303" src="https://github.com/missvickies/tiktok-toronto-food-map/assets/42661718/9ae2e532-f9d8-411a-9505-b5d086be6382">
 
+Try it out here: https://tiktoktorontofoodmap.netlify.app/
+
+
+
+
---
+++ src/components/navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import Paper from '@mui/material/Paper';
+
+export default function Navbar() {
+    const [value, setValue] = React.useState(0);
+    return (
+        <Box>
+            <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+                <BottomNavigation
+                    showLabels
+                    value={value}
+                    onChange={(event, newValue) => {
+                        setValue(newValue);
+                    }}
+                >
+                    <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+                    <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+                    <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+                </BottomNavigation>
+            </Paper>
+        </Box>
+    )
+}
